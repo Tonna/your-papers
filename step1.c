29c0d14d8d89eba45e457ec2b653e60d337d4e7f
Anton Yakovchuk <yakovchuk.a.p@gmail.com>
2020-08-03 07:47:27 +0300

    simplest task described

---
+++ step1.c
@@ -0,0 +1,5 @@
+//background picture
+//shape floating from left to right cyclically in the middle of the screen
+//shape stops in the middle of screen and game waits for input
+//y means share continues moving right
+//n means share goes back to the left
